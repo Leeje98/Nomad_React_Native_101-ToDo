88f995877b03f894c97c418ad58549e968848629
lee Jeong Eun <wjd7dms7@naver.com>
2022-10-19 17:49:31 +0900

    완료 버튼 기능 구현 중

--- App.js
+++ App.js
@@ -19,21 +19,27 @@ import {
 // TouchableWithoutFeedback : 누르는 순간을 인식하나 UI(화면)상으론 아무것도 하지 않는다
 // Pressable : 누르는 순간의 상호작용 / 엄지손가락의 부피를 고려하여 글자주변을 터치해도 인식 되도록 한다(new)
 import { theme } from "./colors";
-import { Fontisto } from '@expo/vector-icons';
+import { Fontisto } from "@expo/vector-icons";
+import { MaterialCommunityIcons } from '@expo/vector-icons';
 
 const STORAGE_KEY = "@toDos";
 
 export default function App() {
   const [working, setWorking] = useState(true);
+  const [finished, setFinished] = useState(false);
   const [text, setText] = useState("");
   const [toDos, setToDos] = useState({});
+
   useEffect(() => {
     loadToDos();
   }, []);
+  
   const travel = () => setWorking(false);
   const work = () => setWorking(true);
+  const finisheding = (key) => setFinished(!finished);
   const onChangeText = (payload) => setText(payload); // 페이로드는 전송되는 데이터 자체를 지칭 / JSON형태
-  const saveToDos = async (toSave) => {                                                                                                  // 
+  const saveToDos = async (toSave) => {
+    //
     // 현재 존재하는 toDos를 string으로 변환 // toDos를 toSave라는 형태로 받겠다
     await AsyncStorage.setItem(STORAGE_KEY, JSON.stringify(toSave)); // JSON.stringify() 사용하여 오브젝트를 스트링으로 변환한다
   }; // key, value
@@ -45,7 +51,7 @@ export default function App() {
       setToDos(JSON.parse(s));
     } catch (e) {
       // saving error
-      console.error('error')
+      console.error("error");
     }
   };
 
@@ -83,7 +89,36 @@ export default function App() {
     ]);
   };
 
-  // console.log(toDos);
+  
+  const finishedToDo = (key) => {
+          const newToDos = { 
+            ...toDos,
+            [key] : { ...toDos[key], ['finished']: !finished }
+          };
+
+          setToDos(newToDos);
+          saveToDos(newToDos);
+  };
+
+  // const finishedToDo = (key) => {
+  //   Alert.alert("완료 된 항목입니까?", "", [
+  //     { text: "취소" },
+  //     {
+  //       text: "확인",
+  //       onPress: () => {
+  //         const newToDos = { 
+  //           ...toDos,
+  //           [key] : { ...toDos[key], ['finished']: true }
+  //         };
+
+  //         setToDos(newToDos);
+  //         saveToDos(newToDos);
+  //       },
+  //     },
+  //   ]);
+  // };  // 대리님이 짜주신 코드
+
+  
   return (
     <View style={styles.container}>
       <Text>Open up App.js to start working on your app!</Text>
@@ -125,9 +160,20 @@ export default function App() {
             toDos[key].working === working ? (
               <View style={styles.toDo} key={key}>
                 <Text style={styles.toDoText}>{toDos[key].text}</Text>
-                <TouchableOpacity onPress={() => deleteToDo(key)}>
-                  <Fontisto name="trash" size={18} color={theme.gray} />
-                </TouchableOpacity>
+                {/* 수정모드일시, ㄴ이곳이 Text가 아닌 text input으로 보여지게 할 것 */}
+                <View style={styles.btnBox}>
+                  
+                  <TouchableOpacity onPress={() => deleteToDo(key)}>
+                    <Fontisto name="trash" size={18} color={theme.btn} />
+                  </TouchableOpacity>
+
+                  <TouchableOpacity style={{flexDirection: "row", marginLeft:10,}} onPress={() => finishedToDo(key)}>
+                  {(toDos[key].finished ?
+                    <Fontisto name="checkbox-active" style={{paddingTop:4}} size={18} color={theme.btn} />
+                    :  <MaterialCommunityIcons name="checkbox-blank-outline" size={25} color={theme.btn} /> )}
+                  </TouchableOpacity>
+
+                </View>
               </View>
             ) : null // 카테고리별로 항목 노출 하게하는 필터링 기능
         )}
@@ -150,6 +196,11 @@ const styles = StyleSheet.create({
   btnText: {
     fontSize: 38,
     fontWeight: "600",
+  },
+  btnBox: {
+    flexDirection: "row",
+    alignItems: "center",
+    justifyContent: "space-between",
   },
   input: {
     backgroundColor: "white",
